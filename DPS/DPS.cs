8b550a61980d9208ef9583a7d84e759f1bf5fdf7
Jere Kuusela <jere.kuusela@iki.fi>
2023-10-05 21:16:00 +0300

    Fixed for the new update.

--- DPS/DPS.cs
+++ DPS/DPS.cs
@@ -8,7 +8,7 @@ public class DPS : BaseUnityPlugin
 {
   public const string GUID = "dps";
   public const string NAME = "DPS";
-  public const string VERSION = "1.5";
+  public const string VERSION = "1.6";
 
   public void Awake()
   {
